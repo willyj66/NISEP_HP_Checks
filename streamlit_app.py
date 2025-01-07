2846e30909610b31d76b8489987809b1f9164948
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-07 16:46:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,10 @@ def cache_lookup():
 lookup_df = cache_lookup()
 variables = lookup_df.name.unique()
 sites = lookup_df.siteNamespace.unique()
-
-
-
-variable = st.multiselect("Variable",variables,default=None)
-variable = None if not variable else variable
 site = st.multiselect("Site",sites,sites[0])
 past_days = st.number_input("Days displayed",1,None,1)
 end_time = datetime(*datetime.now().timetuple()[:3]) # Get today's date from start of day (i.e. midnight)
 start_time = end_time - timedelta(days = past_days) # Look at last day's worth of data
-df = getTimeseries(end_time,start_time,site,variable,auth_url, username, password)
+df = getTimeseries(end_time,start_time,site,variable=None,auth_url, username, password)
 
 st.dataframe(df)
