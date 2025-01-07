40774a6ff52e9e5b52053727fdc0fabee0c1b06b
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-07 16:33:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ start_time = end_time - timedelta(days = 1) # Look at last day's worth of data
 auth_url = st.secrets.get("Login", {}).get("URL", "https://users.carnego.net")
 username = st.secrets.get("Login", {}).get("Username", "")
 password = st.secrets.get("Login", {}).get("Password", "")
+lookup_df = getLookup(auth_url, username, password)
+st.dataframe(lookup_df)
 df = getTimeseries(end_time,start_time,site,variable,auth_url, username, password)
 
 st.dataframe(df)
