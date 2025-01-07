8f7b907b6572ce22c413ac1d358a742774e9b86b
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-07 16:25:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 
 site = ['NISEP01']#,'NISEP02']
-variable = None#['delta_t', 'flow_rate','flow_temp']
+variable = ['delta_t', 'flow_rate','flow_temp']
 end_time = datetime(*datetime.now().timetuple()[:3]) # Get today's date from start of day (i.e. midnight)
 start_time = end_time - timedelta(days = 1) # Look at last day's worth of data
 auth_url = st.secrets.get("Login", {}).get("URL", "https://users.carnego.net")
