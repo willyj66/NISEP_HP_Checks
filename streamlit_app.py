c1c28ce86707973dcacc966191522baf66288e1a
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-07 16:35:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,19 @@ from datetime import datetime, timedelta
 
 
 
-site = ['NISEP01']#,'NISEP02']
-variable = None#['delta_t', 'flow_rate','flow_temp']
+#site = ['NISEP01']#,'NISEP02']
+#variable = None#['delta_t', 'flow_rate','flow_temp']
 end_time = datetime(*datetime.now().timetuple()[:3]) # Get today's date from start of day (i.e. midnight)
 start_time = end_time - timedelta(days = 1) # Look at last day's worth of data
 auth_url = st.secrets.get("Login", {}).get("URL", "https://users.carnego.net")
 username = st.secrets.get("Login", {}).get("Username", "")
 password = st.secrets.get("Login", {}).get("Password", "")
 lookup_df = getLookup(auth_url, username, password)
-st.dataframe(lookup_df)
+variables = lookup_df.name.unique()
+sites = lookup_df.siteNamespace.unique()
+
+variable = st.multiselect(variables)
+site = st.multiselect(sites)
 df = getTimeseries(end_time,start_time,site,variable,auth_url, username, password)
 
 st.dataframe(df)
