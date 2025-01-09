3d224347fa0aebd45c7d47aed258e0cbcd21a45f
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-09 15:18:48 +0000

    dd

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ sites = st.session_state.sites
 
 # --- Sidebar for Control ---
 st.sidebar.title("Controls")
-site = st.sidebar.multiselect("Select Site", sites)
+site = st.sidebar.multiselect("Select Site", sites,None)
 variable = None  # None for downloading all
 past_days = st.sidebar.number_input("Days Displayed", 1, None, 1)
 
