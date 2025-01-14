2fd62d418f902ab56ab0bdd1d7d6fc96b8a629f2
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-14 18:38:44 +0000

    Update 1_Temperature checks.py

--- pages/1_Temperature checks.py
+++ pages/1_Temperature checks.py
@@ -67,8 +67,7 @@ for variable in variable_options:
     relevant_columns = [col for col in temperature_columns if col.startswith(variable)]
     if not relevant_columns:
         continue
-    if variable == "Temperature":
-        continue
+    if variable=="Temperature":
 
     # Prepare the data
     df = df_sesh[["datetime"] + relevant_columns]
