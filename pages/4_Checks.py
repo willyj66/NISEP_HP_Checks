56c02d796120ccb2ff5dd3bcd136ccde8814649c
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-30 13:54:27 +0000

    Update 4_Checks.py

--- pages/4_Checks.py
+++ pages/4_Checks.py
@@ -16,16 +16,12 @@ password = st.secrets.get("Login", {}).get("Password", "")
 @st.cache_data
 def cache_lookup():
     lookup_df = getLookup(auth_url, username, password)
-    return lookup_df.siteNamespace.unique()
-
-all_sites = cache_lookup()
-# Only download data when `past_days` is updated
-if 'df' not in st.session_state:
     # Check the past 30 days
     end_time = datetime(*datetime.now().timetuple()[:3]) 
     start_time = end_time - timedelta(days=30)
 
-    # Fetch the time series data
-    st.session_state.df = getTimeseries(end_time, start_time, None, None, auth_url, username, password)
+    return lookup_df.siteNamespace.unique(), getTimeseries(end_time, start_time, None, None, auth_url, username, password)
 
-st.dataframe(st.session_state.df)
+all_sites, st.session_state.nisep_df = cache_lookup()
+
+st.dataframe(st.session_state.nisep_df)
