74c59f304f66047aaff02a2d7d20b51aaec4b31f
Maxim <118287392+moweyssi@users.noreply.github.com>
2025-01-30 19:27:19 +0000

    b

--- checks_functions.py
+++ checks_functions.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import pytz
 import streamlit as st
 
-def process_temperature_and_delta_t_data(df, past_days, bounds, site_names, subsample_freq='10T'):
+def process_temperature_and_delta_t_data(df, past_days, bounds, site_names, subsample_freq='10min'):
     """
     Processes temperature and Delta T time series data for visualization.
     
